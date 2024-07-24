f06682958a06268e6b0a77b8ee6d11f9329e8be0
Irina <munteauirina@gmail.com>
2024-07-24 16:43:42 +0200

    configPostman
    config per postman, creat jwt e sistemato Store

---
+++ GestioneAppuntamenti/backend/irina/irina/src/main/java/lgsf/boundary/VetturaResources.java
@@ -0,0 +1,168 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lgsf.boundary;
+
+/**
+ *
+ * @author Stage
+ */
+@Path("users")
+@Tag(name = "Gestione Users", description = "Permette di gestire gli utenti di bkmapp")
+@DenyAll
+public class UsersResources {
+    
+    @Inject
+    private UserStore storeuser;
+    
+    @Context
+    ResourceContext rc;
+    
+    @Context
+    UriInfo uriInfo;
+    
+    @Inject
+    private JWTManager jwtManager;
+    
+    @Inject
+    private JsonWebToken token;
+    
+    @Claim(value = "sub")
+    private String sub;
+        
+   
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Restituisce l'elenco di tutti gli utenti")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Elenco ritornato con successo"),
+        @APIResponse(responseCode = "404", description = "Elenco non trovato")
+    })
+    @RolesAllowed({"Admin","User"})
+    public List<User> all(@DefaultValue("1") @QueryParam("page") int page, @DefaultValue("10") @QueryParam("size") int size) {
+        System.out.println(token);
+        return storeuser.all();
+    }
+    
+    
+    @GET
+    @Path("allslice")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Restituisce l'elenco con informazioni ridotte di tutti gli utenti")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Elenco ritornato con successo"),
+        @APIResponse(responseCode = "404", description = "Elenco non trovato")
+    })
+    @PermitAll
+    public JsonArray allSlice() {
+        //System.out.println(token);
+        return storeuser.all().stream().map(User::toJsonSliceName).collect(JsonCollectors.toJsonArray());
+    }
+    
+    
+        
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Restituisce la risorsa utente identificata dall'ID")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Utente ritornato con successo"),
+        @APIResponse(responseCode = "404", description = "Utente non trovato")
+    })
+    @RolesAllowed({"Admin","User"})
+    public User find(@PathParam("id") Long id) {
+        return storeuser.find(id).orElseThrow(() -> new NotFoundException("user non trovato. id=" + id));
+    }
+    
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Permette la registrazione di un nuovo utente")
+    @APIResponses({
+        @APIResponse(responseCode = "201", description = "Nuovo utente creato con successo"),
+        @APIResponse(responseCode = "404", description = "Creazione di utente fallito")
+    })
+    @PermitAll
+    public Response create(@Valid User entity) {
+        
+        if(storeuser.findUserbyLogin(entity.getEmail()).isPresent()){
+            
+           return Response.status(Response.Status.PRECONDITION_FAILED).build();
+        }
+        
+        User saved = storeuser.save(entity);
+        
+        return Response.status(Response.Status.CREATED)
+                .entity(saved)
+                .build();
+}
+    
+    
+    @POST
+    @Path("login")
+    @Operation(description = "Permette fare login e ristituisce il token valido")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Login fatto con successo"),
+        @APIResponse(responseCode = "404", description = "Login fallito")
+
+    })
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @PermitAll
+    public JsonObject login (@Valid Credential credential){
+        
+        User u = storeuser.login(credential).orElseThrow(() -> new NotAuthorizedException("User non Authorized",  
+                                                                       Response.status(Response.Status.UNAUTHORIZED).build()));
+        String jwt = jwtManager.generate(u);
+         
+        return  Json.createObjectBuilder()
+                .add("mail", u.getEmail())
+                .add("token",jwt)
+                .add("userid", u.getId())
+                .add("first_name", u.getFirstName())
+                .add("last_name", u.getLastName())
+                .add("role", u.getRoleuser().toString())
+                .build();
+    }
+    
+    
+    
+    @DELETE
+    @Path("{id}")
+    @Operation(description = "Elimina una risorsa Utente tramite l'ID")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Utente eliminato con successo"),
+        @APIResponse(responseCode = "404", description = "Utente non trovato")
+
+    })
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("Admin")
+    public Response delete(@PathParam("id") Long id) {
+        User found = storeuser.find(id).orElseThrow(() -> new NotFoundException("user non trovato. id=" + id));
+        storeuser.remove(found);
+        return Response.status(Response.Status.OK)
+                .build();
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Aggiorna i dati dell'utente")
+    @APIResponses({
+        @APIResponse(responseCode = "200", description = "Utente aggirnato con successo"),
+        @APIResponse(responseCode = "404", description = "Aggiornamento falito")
+            
+    })
+    @RolesAllowed("Admin")
+    public User update(@PathParam("id") Long id, @Valid User entity) {
+        User found = storeuser.find(id).orElseThrow(() -> new NotFoundException("user non trovato. id=" + id));
+        entity.setId(id);
+        return storeuser.update(entity);
+    }
+   
+    
+    
+}
