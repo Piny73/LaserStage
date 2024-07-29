2f48e225f5353129b02f73fe8fb55c032463fe3b
Irina <munteauirina@gmail.com>
2024-07-29 14:47:43 +0200

    add adapter
    add TypeAdapter

---
+++ GestioneAppuntamenti/backend/irina/irina/src/main/java/project_lgsf/entity/adapter/UserTypeAdapter.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project_lgsf.entity.adapter;
+
+/**
+ *
+ * @author Stage
+ */
+import javax.json.bind.adapter.JsonbAdapter;
+import javax.json.JsonObject;
+import javax.inject.Inject;
+import javax.ws.rs.NotFoundException;
+import project_lgsf.entity.User;
+import project_lgsf.store.UserStore;
+
+
+/**
+ *
+ * @author AndreLima
+ */
+public class UserTypeAdapter implements JsonbAdapter<User, JsonObject>  {
+    
+    @Inject
+    UserStore store;
+
+    @Override
+    public JsonObject adaptToJson(User entity) throws Exception {
+        return entity.toJsonSlice();
+    }
+
+    @Override
+    public User adaptFromJson(JsonObject json) throws Exception {
+        if (!json.containsKey("id")) {
+            return null;
+        }
+        return store.find(json.getJsonNumber("id").longValue()).orElseThrow(() -> new NotFoundException("UserTypeAdapter.adaptFromJson not found"));
+    }
+    
+    
+    
+}
