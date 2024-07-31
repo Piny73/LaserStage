efd1c48f334837b3a92050fd55cb58f7efdb857e
Piny73 <Piny73@gmail.com>
2024-07-31 15:02:02 +0200

    31/07 ore
    agg

---
+++ ricettedinonnaMaria/Front End/Fricettedinonnamaria/src/app/core/api.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl = 'http://localhost:8080/recipe/api'; // URL base da sua API
+
+  constructor(private http: HttpClient) { }
+
+  // Método POST genérico
+  post(endpoint: string, data: any, headers?: HttpHeaders): Observable<any> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return this.http.post(url, data, { headers });
+  }
+
+  // Método GET genérico
+  get(endpoint: string, headers?: HttpHeaders): Observable<any> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return this.http.get(url, { headers });
+  }
+
+  // Método PUT genérico
+  put(endpoint: string, data: any, headers?: HttpHeaders): Observable<any> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return this.http.put(url, data, { headers });
+  }
+
+  // Método DELETE genérico
+  delete(endpoint: string, headers?: HttpHeaders): Observable<any> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return this.http.delete(url, { headers });
+  }
+}
+
---
+++ ricettedinonnaMaria/Front End/Fricettedinonnamaria/src/app/features/features.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports: [LoginComponent]
+})
+export class FeaturesModule { }
