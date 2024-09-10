a5e7b806ad2787c61e2c5e861001f9543d27a597
Irina <munteauirina@gmail.com>
2024-09-10 09:56:10 +0200

    modifica delete

--- GestioneAppuntamenti/backend/irina/irina/src/main/java/project_lgsf/boundary/UsersResources.java
+++ GestioneAppuntamenti/backend/irina/irina/src/main/java/project_lgsf/boundary/UsersResources.java
@@ -43,10 +43,7 @@ import project_lgsf.security.JWTManager;
 import project_lgsf.store.UserStore;
 import project_lgsf.entity.User;
 
-/**
- *
- * @author AndreLima
- */
+
 @Path("users")
 @Tag(name = "Gestione Users", description = "Permette di gestire gli admin di bkmapp")
 @DenyAll
