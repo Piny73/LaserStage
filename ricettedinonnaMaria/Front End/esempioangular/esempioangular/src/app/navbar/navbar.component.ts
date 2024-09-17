88b89c79517dc774b568d72fb8f7da434c5d342d
Piny73 <Piny73@gmail.com>
2024-09-17 11:05:38 +0200

    17/09 inizio agg

--- ricettedinonnaMaria/Front End/esempioangular/esempioangular/src/app/navbar/navbar.component.ts
+++ ricettedinonnaMaria/Front End/esempioangular/esempioangular/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contact } from '../models/contact.model';  // Assicurati di importare il modello
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,23 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-createContact() {
-throw new Error('Method not implemented.');
-}
-createRecipe() {
-throw new Error('Method not implemented.');
-}
-createIngredient() {
-throw new Error('Method not implemented.');
-}
-
   title = '';  // Definisci la proprietà title
   showLogin = false;
+  // Array per contenere i contatti creati
+  contacts: Contact[] = [];
+  showForm: boolean = false;  // Variabile per gestire la visibilità del form
 
   toggleLogin() {
-    
     this.showLogin = !this.showLogin;  // Alterna la visibilità del form di login
   }
+
+  createContact(nome: string, cognome: string, email: string) {
+    const newContact = new Contact(nome, cognome, email);
+    this.contacts.push(newContact);  // Aggiungi il contatto all'array
+    console.log('Contact created:', newContact);  // Stampa il contatto creato
+    this.showForm = false; // Nascondi il form dopo la creazione del contatto
+  }
+
+  // Metodo per mostrare il form
+  toggleForm() {
+    this.showForm = !this.showForm;  // Alterna tra true e false
+  }
+    createRecipe() {
+    throw new Error('Method not implemented.');
+    }
+    createIngredient() {
+    throw new Error('Method not implemented.');
+    }
   }
   
 
