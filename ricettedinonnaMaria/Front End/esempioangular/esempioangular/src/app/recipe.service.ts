a2f106e1182027889f528ce32f1021f66cdee7b6
Piny73 <Piny73@gmail.com>
2024-09-20 11:05:48 +0200

    agg 20/09/2024 tutto stilisticamente perfetto!

---
+++ ricettedinonnaMaria/Front End/esempioangular/esempioangular/src/app/recipe.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private apiUrl = 'http://localhost:8080/api/recipe'; // URL del backend
+}
