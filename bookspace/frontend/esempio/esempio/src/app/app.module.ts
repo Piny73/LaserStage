025799d6e37ab13bc884adacf4bbb5f0b1aea2e5
saramartinetti <saramartinetti05@gmail.com>
2024-09-27 14:55:10 +0200

    ok
    ok

--- bookspace/frontend/esempio/esempio/src/app/app.module.ts
+++ bookspace/frontend/esempio/esempio/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LayoutModule,
+    HttpClientModule,
+    FormsModule,
     LayoutModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ bookspace/frontend/esempio/esempio/src/app/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:4200'; // URL del tuo backend
+  private token: string | null = null;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(email: string, password: string) {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, { email, password })
+      .subscribe(response => {
+        this.token = response.token;
+        localStorage.setItem('token', this.token);
+        this.router.navigate(['/']); // Redirigi l'utente dopo il login
+      }, error => {
+        alert('Errore durante il login: ' + error.error);
+      });
+  }
+
+  logout() {
+    this.token = null;
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return this.token !== null;
+  }
+}
