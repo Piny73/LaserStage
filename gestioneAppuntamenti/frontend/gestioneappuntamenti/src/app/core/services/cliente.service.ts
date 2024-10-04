f3b9aef90a24cf8df90d356dfa3e9a0c82e3ac44
Irina <160396544+MunteanuIrina@users.noreply.github.com>
2024-10-04 10:57:33 +0200

    ok

--- gestioneAppuntamenti/frontend/gestioneappuntamenti/src/app/core/services/cliente.service.ts
+++ gestioneAppuntamenti/frontend/gestioneappuntamenti/src/app/core/services/cliente.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+<<<<<<< Updated upstream
 import { map, Observable } from 'rxjs';
+=======
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+>>>>>>> Stashed changes
 import { Cliente } from '../models/cliente.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClienteService {
+<<<<<<< Updated upstream
   private apiUrl = 'http://localhost:8080/clienti';
 
   constructor(private http: HttpClient) {}
@@ -38,3 +45,27 @@ export class ClienteService {
   }
 }
 
+=======
+
+  private apiUrl = 'http://localhost:8080/clienti';  // Modifica l'URL in base al tuo backend
+
+  constructor(private http: HttpClient) {}
+
+  getClienti(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.apiUrl);
+  }
+
+  addCliente(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.apiUrl, cliente);
+  }
+
+  updateCliente(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.apiUrl}/${cliente.id}`, cliente);
+  }
+
+  deleteCliente(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
+
+>>>>>>> Stashed changes
