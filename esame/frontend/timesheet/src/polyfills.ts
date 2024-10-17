26e6907ef17dfe98796c9493cf664fb9575d8ac7
Irina <munteauirina@gmail.com>
2024-10-17 16:11:26 +0200

    aggiornamento

---
+++ esame/frontend/timesheet/src/polyfills.ts
@@ -0,0 +1,21 @@
+// Importa il core-js polyfill per le funzionalità ES6/ES2015
+import 'core-js/es/array';
+import 'core-js/es/function';
+import 'core-js/es/map';
+import 'core-js/es/math';
+import 'core-js/es/number';
+import 'core-js/es/object';
+import 'core-js/es/parse-float';
+import 'core-js/es/parse-int';
+import 'core-js/es/promise';
+import 'core-js/es/reflect';
+import 'core-js/es/set';
+import 'core-js/es/string';
+import 'core-js/es/symbol';
+import 'core-js/es/weak-set';
+
+
+import 'zone.js'; // Assicurati che questa sia l'ultima importazione nel file
+
+
+
